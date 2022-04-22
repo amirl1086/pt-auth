e31bbaf5f21547dbaf9bba9fc68d6af249ac5f45
amirl1086 <amirl1086@gmail.com>
2022-04-22 13:55:10 +0300

    added server.js

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ const port = config.server.port;
 const routes = require('./lib/routes');
 
 
+// middlewares
 app.use(express.json()); // parse requests of content-type - application/json
 app.use(express.urlencoded({ extended: true })); // parse requests of content-type - application/x-www-form-urlencoded
 app.use(cors({origin: `http://${config.server.host}:${port}`}));
-
-const db = require('./lib/mongo/db');
 
 app.use((req, res, next) => {
     console.log('server headers middleware');
@@ -25,27 +24,21 @@ app.use((req, res, next) => {
     });
     next();
 });
-
-// routes
 app.use('/api', routes);
-
-//errors middleware
 app.use((err, req, res, next) => {
     // console.log('error handler middleware');
-    console.error(`middleware error: ${JSON.stringify(err, '', 4)}`);
+    console.error('middleware error: ', err);
     res.status(err.statusCode).send(err.message);
 });
 
-
+// server startup
 (async () => { // eslint-disable-line no-unexpected-multiline
     try {
-        await db.connect('admin');
         app.listen(port, () => {
             console.log(`auth app listening at http://localhost:${port}`);
         });
     } catch (err) {
-        console.error(`error starting server, ${err}`);
+        console.error('error starting server: ', err);
         process.exit(1);
     }
 })();
-
